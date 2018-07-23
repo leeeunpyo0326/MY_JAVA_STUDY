044faf44ed7df504630f28d6fdcf7b0edf8dd1f2
admin <admin@10.137.138.164>
2018-07-23 12:33:07 +0900

    UTF-8로 인코딩형식을 바꿈

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,7 +1,16 @@
 package icehs.science.chapter02;
 
 public class HelloWorld {
-	
+	/**
+	 * java야 
+	 * @param args
+	 */
+	/*
+	 * 이 프로그램은 선인고등학교 Java 응용프로그래밍 수업에서 만들어졌습니다.
+	 * 만든사람 : 이은표
+	 * 만든 날짜 : 2018년 7월 23일
+	 * *버전 : 
+	 */
 	public static void main(String[] args) {
 		//나의 첫번째 프로그램
 		System.out.println("Hello World");
