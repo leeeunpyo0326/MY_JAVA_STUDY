e22f5b48d3f38fa6234ad1b5a9969d44f7d0ddbc
admin <admin@10.137.138.164>
2018-07-23 12:22:25 +0900

    Java의 주석은 세가지가 있음 // /*~*/ /**~*/

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,9 +1,11 @@
 package icehs.science.chapter02;
 
 public class HelloWorld {
-
+	
 	public static void main(String[] args) {
-		System.out.println("Hello 이은푯!!");
+		//나의 첫번째 프로그램
+		System.out.println("Hello World");
+		System.out.println("이은표!!");
 
 	}
 
