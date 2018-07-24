f3a2370ad05dae96b251b9c759e08611cf710bf1
admin <admin@10.211.1.69>
2018-07-24 16:25:43 +0900

    CHAPTER05

---
+++ src/icehs/science/chapter04/GuGuDanOddNumberTest.java
@@ -0,0 +1,11 @@
+package icehs.science.chapter04;
+
+public class GuGuDanOddNumberTest {
+	
+	public static void main(String [ ] args) {
+		
+	}
+	
+	
+
+}
---
+++ src/icehs/science/chapter04/WhilePrintTest.java
@@ -0,0 +1,12 @@
+package icehs.science.chapter04;
+
+public class WhilePrintTest {
+	public static void main(String [] args) {
+		int i = 1;
+		while(i <= 10) {
+			System.out.println(i +".While 실습입니다.");
+			i++;
+		}
+	}
+
+}
